e5876a826409bf0550b8c27f3b9c50faa777197e
TsawadaTsuna <42613553+TsawadaTsuna@users.noreply.github.com>
2021-03-06 19:34:22 -0600

    El scroll del 6 de marzo

---
+++ app/src/main/java/com/example/aznotes/EditorNotaActivity.java
@@ -0,0 +1,14 @@
+package com.example.aznotes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditorNotaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editor_nota);
+    }
+}
